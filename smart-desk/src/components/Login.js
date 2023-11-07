53e9dd1995b266e6b5ae5d78b9f1a28a12ed7503
Pratyush gogoi <96740074+Its-Pratyush@users.noreply.github.com>
2023-11-07 23:06:52 +0530

    Update Login.js

--- smart-desk/src/components/Login.js
+++ smart-desk/src/components/Login.js
@@ -7,7 +7,7 @@ const LoginForm = () => {
   const [credentials, setCredentials] = useState({ email: "", password: "" });
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:8000/api/auth/login", {
+    const response = await fetch("https://smart-desk-mern.vercel.app/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
