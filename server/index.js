9492853697c3e61d7ba9134959d51eb3cba4b88a
Pratyush gogoi <96740074+Its-Pratyush@users.noreply.github.com>
2023-11-09 18:31:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,29 +2,14 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const connectToMongo = require("./db");
-const cors = require("cors"); // Correct the import statement
+const cors = require("cors");
 
 connectToMongo();
-
-
-
-
-// Configure CORS options
-const corsOptions = {
-  origin: ["https://smart-desk-sj4n.vercel.app"]
-  methods: ["POST", "PUT", "DELETE", "GET"],
-  credentials: true,
-};
-
-// Use cors middleware with options
-app.use(cors(corsOptions));
-
+app.use(cors());
 app.use(express.json());
 
-// Available routes
-app.get("/", (req,res)=>{
-  res.json("Hello");
-})
+//available routes
+
 app.use("/api/auth", require("./Routes/auth"));
 app.use("/api/notes", require("./Routes/notes"));
 app.use("/api/todos", require("./Routes/todo"));
@@ -32,4 +17,3 @@ app.use("/api/todos", require("./Routes/todo"));
 app.listen(port, () => {
   console.log(`smart desk backend http://localhost:${port}`);
 });
-
